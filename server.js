0330e5d4baa5261f43982c47f9c1c0eff7c7f984
akhthomas <ms.kellanthomas@gmail.com>
2022-04-20 15:16:03 -0700

    first middleware

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// middleware
+app.use(function(req, res, next) {
+  console.log('Hello Fellow Reader!');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
